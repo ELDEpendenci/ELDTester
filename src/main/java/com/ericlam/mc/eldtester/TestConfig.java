a2c28d920930dbc43a1ca83ee9b016ac7349ca04
eric2788 <tsukiko852@gmail.com>
2020-10-15 11:25:32 +0800

    update for api v0.0.5 and documentation

--- src/main/java/com/ericlam/mc/eldtester/TestConfig.java
+++ src/main/java/com/ericlam/mc/eldtester/TestConfig.java
@@ -8,4 +8,35 @@ import org.bukkit.ChatColor;
 public class TestConfig extends Configuration {
     public boolean useMySQL;
     public String host;
+    public String name;
+    public int number;
+    public boolean bool;
+    public Box box;
+
+    public static class Box {
+        public String name;
+        public int size;
+        public ChatColor color;
+
+
+        @Override
+        public String toString() {
+            return "Box{" +
+                    "name='" + name + '\'' +
+                    ", size=" + size +
+                    ", color=" + color +
+                    '}';
+        }
+    }
+
+
+    @Override
+    public String toString() {
+        return "TestConfig{" +
+                "name='" + name + '\'' +
+                ", number=" + number +
+                ", bool=" + bool +
+                ", box=" + box +
+                '}';
+    }
 }
