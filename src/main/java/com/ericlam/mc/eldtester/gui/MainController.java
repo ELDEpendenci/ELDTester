f91332add8b9ceddd8d5a8a078a55a76711bc577
eric2788 <tsukiko852@gmail.com>
2021-09-01 10:52:29 +0800

    update for eld-mvc

--- src/main/java/com/ericlam/mc/eldtester/gui/MainController.java
+++ src/main/java/com/ericlam/mc/eldtester/gui/MainController.java
@@ -2,6 +2,8 @@ package com.ericlam.mc.eldtester.gui;
 
 import com.ericlam.mc.eldgui.controller.UIController;
 import com.ericlam.mc.eldgui.event.ClickMapping;
+import com.ericlam.mc.eldgui.lifecycle.PostUpdateView;
+import com.ericlam.mc.eldgui.lifecycle.PreDestroyView;
 import com.ericlam.mc.eldgui.view.BukkitView;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.ClickType;
@@ -19,6 +21,17 @@ public class MainController {
     @ClickMapping(pattern = 'A', view = MainView.class)
     public void onClickA(Player player) {
         player.sendMessage("activated !!!!");
+    }
+
+
+    @PreDestroyView(MainView.class)
+    public void onDestroyView(Player player){
+        player.sendMessage("pre destroy view for main view");
+    }
+
+    @PostUpdateView(MainView.class)
+    public void postUpdateView(Player player){
+        player.sendMessage("updated to main view");
     }
 
 }
--- src/main/java/com/ericlam/mc/eldtester/gui/MainView.java
+++ src/main/java/com/ericlam/mc/eldtester/gui/MainView.java
@@ -1,6 +1,7 @@
 package com.ericlam.mc.eldtester.gui;
 
 import com.ericlam.mc.eldgui.component.factory.ButtonFactory;
+import com.ericlam.mc.eldgui.component.factory.NumInputFactory;
 import com.ericlam.mc.eldgui.view.UIContext;
 import com.ericlam.mc.eldgui.view.UseTemplate;
 import com.ericlam.mc.eldgui.view.View;
@@ -17,10 +18,13 @@ public class MainView implements View<String> { // 此界面裝載 String 作為
     @Override
     public void renderView(String s, UIContext context) {
         ButtonFactory button = context.factory(ButtonFactory.class); //獲取 按鈕組件工廠
+        NumInputFactory numInput = context.factory(NumInputFactory.class);
         context.pattern('A') // 指定 Pattern A
                 .components( // 放入組件
-                        button.icon(Material.DIAMOND_BLOCK) // 設置鑽石方塊
-                                .title(s) // 設置顯示
+                        numInput.icon(Material.DIAMOND_BLOCK) // 設置鑽石方塊
+                                .label(s) // 設置顯示
+                                .useNumberType(Double.class)
+                                .then()
                                 .create() // 創建組件
                 );
     }
